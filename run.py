6d2e4fccd294239ceb489c3a9fc98592d2e6124e
aishaadamu04 <ayshagiade@gmail.com>
2026-07-01 13:37:54 +0100

    Fix blueprint import path

--- run.py
+++ run.py
@@ -1,6 +1,17 @@
-from app import create_app
+#from app import create_app
+from flask import Flask
 
-app = create_app('development')
+def create_app():
+    app = Flask(__name__)
+    from app.student.routes import main_bp
+    app.register_blueprint(main_bp)
 
-if __name__ == '__main__':
-    app.run(port=5001)
+    return app
+
+
+# from app import create_app
+
+# app = create_app('development')
+
+# if __name__ == '__main__':
+#     app.run(port=5001)

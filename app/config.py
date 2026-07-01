07e9c2b0a6a61f3c506edf578ae48acdaa101857
aishaadamu04 <ayshagiade@gmail.com>
2026-07-01 14:32:44 +0100

    fix run.py and database path for render

--- app/config.py
+++ app/config.py
@@ -6,7 +6,7 @@ load_dotenv()  # reads your .env file into environment variables
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY', 'dev-fallback-key-change-in-prod')
-    DATABASE = os.path.join(os.path.dirname(__file__), 'instance', 'bthub.db')
+    DATABASE = os.path.join('/tmp', 'bthub.db')
     DEBUG = False
 
 class DevelopmentConfig(Config):
--- run.py
+++ run.py
@@ -1,16 +1,6 @@
-#from app import create_app
-# from flask import Flask
-
-# def create_app():
-#     app = Flask(__name__)
-#     from app.student.routes import student_bp
-#     app.register_blueprint(student_bp)
-
-#     return app
-
 from app import create_app
 
-app = create_app('development')
+app = create_app()
 
 if __name__ == '__main__':
     app.run(port=5001)

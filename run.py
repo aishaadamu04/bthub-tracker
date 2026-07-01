73cd1ff154911d6f88575d34d07d14896773dffa
aishaadamu04 <ayshagiade@gmail.com>
2026-07-01 14:28:16 +0100

    fix entry points - remove duplicate create_app

--- run.py
+++ run.py
@@ -1,17 +1,16 @@
 #from app import create_app
-from flask import Flask
+# from flask import Flask
 
-def create_app():
-    app = Flask(__name__)
-    from app.student.routes import student_bp
-    app.register_blueprint(student_bp)
+# def create_app():
+#     app = Flask(__name__)
+#     from app.student.routes import student_bp
+#     app.register_blueprint(student_bp)
 
-    return app
+#     return app
 
+from app import create_app
 
-# from app import create_app
+app = create_app('development')
 
-# app = create_app('development')
-
-# if __name__ == '__main__':
-#     app.run(port=5001)
+if __name__ == '__main__':
+    app.run(port=5001)
